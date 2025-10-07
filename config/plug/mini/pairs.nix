6ecbba881849f6de7eb70175036cb91a2d9c15b2
Ryder-C <rydercasazza@gmail.com>
2025-10-07 14:38:47 -0700

    typst fix

--- config/plug/mini/pairs.nix
+++ config/plug/mini/pairs.nix
@@ -1,11 +1,25 @@
 {
-  plugins.mini.modules.pairs = {
-    mappings = {
-      "$" = {
-        action = "closeopen";
-        pair = "$$";
-        neigh_pattern = "[^\\].";
-      };
-    };
-  };
+  plugins.mini.modules.pairs = {};
+
+  extraConfigLua = ''
+    local function configure_typst_pairs(buf)
+      local ok, mini_pairs = pcall(require, 'mini.pairs')
+      if not ok or type(mini_pairs.map_buf) ~= 'function' then
+        return
+      end
+
+      mini_pairs.map_buf(buf, 'i', '$', {
+        action = 'closeopen',
+        pair = '$$',
+        neigh_pattern = '[^\\].',
+      })
+    end
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'typst', 'typ' },
+      callback = function(args)
+        configure_typst_pairs(args.buf)
+      end,
+    })
+  '';
 }
--- config/plug/utils/markview.nix
+++ config/plug/utils/markview.nix
@@ -1,5 +1,10 @@
 {
   plugins.markview = {
     enable = true;
+    settings.preview.filetypes = [
+      "markdown"
+      "quarto"
+      "rmd"
+    ];
   };
 }
